65717ea56e9a276e9d75f7ebef37d6369eb8969e
Elliot Sayes <elliot@sayes.cloud>
2022-07-05 17:03:54 +0800

    Remove example code and scaffold screens

---
+++ lib/screens/create_message_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CreateMessageScreen extends StatelessWidget {
+  const CreateMessageScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
