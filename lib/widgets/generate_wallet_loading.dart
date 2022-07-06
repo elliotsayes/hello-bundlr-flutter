b7cce5b3584f6851c6b03389c9fd35ee67f77a45
Elliot Sayes <elliot@sayes.cloud>
2022-07-06 11:54:01 +0800

    More realistic duration estimate message

--- lib/widgets/generate_wallet_loading.dart
+++ lib/widgets/generate_wallet_loading.dart
@@ -1,7 +1,12 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:loading_animations/loading_animations.dart';
 
 class GenerateWalletLoading extends StatelessWidget {
+  final String durationEstimate = kIsWeb
+      ? 'may take up to a minute on web'
+      : 'should only take a few seconds';
+
   const GenerateWalletLoading({
     Key? key,
   }) : super(key: key);
@@ -12,7 +17,7 @@ class GenerateWalletLoading extends StatelessWidget {
       padding: const EdgeInsets.all(50),
       child: Column(
         children: [
-          const Text('Generating wallet...\r\n(may take several seconds)'),
+          Text('Generating wallet...\r\n($durationEstimate)'),
           Expanded(
             child: FittedBox(
               child: Padding(
